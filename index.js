d5a88951601842f56df9ef63e1e3d41d9e989dcd
Kurt <kurt.bauer55@gmail.com>
2018-01-14 00:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function whisper(string) {
 function logShout(string) {
   return console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
