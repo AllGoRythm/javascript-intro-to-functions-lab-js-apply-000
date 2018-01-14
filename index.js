6c408b9d69a6080367bdbd8941637764dcd9f3e3
Kurt <kurt.bauer55@gmail.com>
2018-01-14 00:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
 
   if (string.toLowerCase() === lowerCase) {
     return 'I can\'t hear you!'
-  } elseif (string.toUpperCase() === lowerCase) {
+  } else (string.toUpperCase() === lowerCase) {
     return 'YES INDEED!'
   }
 
